b6a0dccd463ebe545bc083acd7c26878527d8de8
KermitExile <liucm.it@gmail.com>
2018-07-26 22:41:11 +0800

    feat(anydoor): init anydoor v0.1.1

---
+++ src/helper/route.js
@@ -0,0 +1,70 @@
+const fs = require('fs')
+const handlebars = require('handlebars')
+const {promisify} = require('util')
+const stat = promisify(fs.stat)
+const readdir = promisify(fs.readdir)
+const path = require('path')
+const mime = require('./mime')
+const compress = require('./compress')
+const range = require('./range')
+const isFresh = require('./cache')
+
+const tplPath = path.join(__dirname, '../template/dir.tpl')
+const source = fs.readFileSync(tplPath)
+const template = handlebars.compile(source.toString())
+
+module.exports = async function (req, res, filePath, config) {
+    try {
+        const stats = await stat(filePath)
+        if (stats.isFile()) {
+            const contentType = mime(filePath)
+            
+            res.setHeader('Content-Type', contentType)
+            // fs.readFile(filePath, (err, data) => {
+            //     res.end(data);
+            // })
+
+            if (isFresh(stats, req, res)) {
+                res.statusCode = 304
+                res.end()
+                return
+            }
+
+            let rs
+            const {code, start, end} = range(stats.size, req, res)
+            if (code === 200) {
+                res.statusCode = 200
+                rs = fs.createReadStream(filePath)
+            } else {
+                res.statusCode = 206
+                rs = fs.createReadStream(filePath, {start, end})
+            }
+            //let rs = fs.createReadStream(filePath)
+            if (filePath.match(config.compress)) {
+                rs = compress(rs, req, res)
+            }
+            rs.pipe(res)
+        } else if (stats.isDirectory()) {
+            const files = await readdir(filePath)
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'text/html')
+            const dir = path.relative(config.root, filePath)
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files: files.map(file => {
+                    return {
+                        file,
+                        icon:mime(file)
+                    }
+                })
+            }
+            res.end(template(data))
+        }
+    } catch (error) {
+        console.error(error)
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/plain')
+        res.end(`${filePath} is not a directory or file`)
+    }
+}
